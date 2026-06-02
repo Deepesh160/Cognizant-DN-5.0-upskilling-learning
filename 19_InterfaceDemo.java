861dd174a3bcb0e5793bfe65533515c691426303
Deepesh160 <dangi.deepesh16@gmail.com>
2026-06-02 17:58:55 +0530

    Rename 19_InterfaceDemo.java to Module3.Sol/19_InterfaceDemo.java

--- 19_InterfaceDemo.java
+++
@@ -1,1 +0,0 @@
-interface Playable{void play();} class Guitar implements Playable{public void play(){System.out.println("Guitar");}} class Piano implements Playable{public void play(){System.out.println("Piano");}} public class InterfaceDemo{public static void main(String[]a){new Guitar().play();new Piano().play();}}
