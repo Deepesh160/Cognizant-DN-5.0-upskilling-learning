375c66889f041829b265f657df4559deb3d7c6fd
Deepesh160 <dangi.deepesh16@gmail.com>
2026-06-02 17:35:44 +0530

    Rename 10_NumberGuessingGame.java to Module3.Sol/10_NumberGuessingGame.java

--- 10_NumberGuessingGame.java
+++
@@ -1,1 +0,0 @@
-import java.util.*; public class NumberGuessingGame{public static void main(String[]a){int r=new Random().nextInt(100)+1;Scanner s=new Scanner(System.in);int g;do{g=s.nextInt();System.out.println(g<r?"Low":g>r?"High":"Correct");}while(g!=r);}}
