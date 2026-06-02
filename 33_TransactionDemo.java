4419db61ed427853b449e0afa5b813f36abc3a5d
Deepesh160 <dangi.deepesh16@gmail.com>
2026-06-02 18:05:05 +0530

    Rename 33_TransactionDemo.java to Module3.Sol/33_TransactionDemo.java

--- 33_TransactionDemo.java
+++
@@ -1,1 +0,0 @@
-public class TransactionDemo{public static void main(String[]a){System.out.println("Transaction Example");}}
---
+++ Module3.Sol/33_TransactionDemo.java
@@ -0,0 +1,1 @@
+public class TransactionDemo{public static void main(String[]a){System.out.println("Transaction Example");}}
