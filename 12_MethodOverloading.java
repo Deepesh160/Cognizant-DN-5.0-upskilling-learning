abc40ec095ad6165415ef146840ecb5edbc5b008
Deepesh160 <dangi.deepesh16@gmail.com>
2026-06-02 17:36:39 +0530

    Rename 12_MethodOverloading.java to Module3.Sol/12_MethodOverloading.java

--- 12_MethodOverloading.java
+++
@@ -1,1 +0,0 @@
-public class MethodOverloading{static int add(int a,int b){return a+b;} static double add(double a,double b){return a+b;} static int add(int a,int b,int c){return a+b+c;} public static void main(String[]a){}}
---
+++ Module3.Sol/12_MethodOverloading.java
@@ -0,0 +1,1 @@
+public class MethodOverloading{static int add(int a,int b){return a+b;} static double add(double a,double b){return a+b;} static int add(int a,int b,int c){return a+b+c;} public static void main(String[]a){}}
